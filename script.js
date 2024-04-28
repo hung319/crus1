8c9baceca641250063f3299848245de5f17c2951
Ross <83570379+thanhtayt@users.noreply.github.com>
2024-04-28 09:09:16 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function showResult() {
+    var result = document.getElementById("result");
+    result.style.display = "block";
+
+    // Thay đổi nội dung kết quả theo ý muốn
+    result.innerHTML = "<p>...</p>";
+}
